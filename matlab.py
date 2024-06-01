86925248a951d8f8b3b8fb2a51f5c858cc52c091
AAMENA123 <aamianar4@gmail.com>
2024-06-01 18:34:37 +0530

    matlab

---
+++ matlab.py
@@ -0,0 +1,8 @@
+name = input("Enter your name:")
+city = input("Enter the city you belong to:")
+state =input("Enter the state you belong to:")
+country = input("Enter the country you belong to:")
+language = input("enter the programming language you are currently learning:")
+matlib = f"My name is {name}. I am from {city}, {state}, {country}. Currently , \
+I am learning{language} programming language"
+print(matlib)
